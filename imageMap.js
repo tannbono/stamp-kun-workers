79e5a3ce3cb853839c2de9010d340f859fb2c231
tannbono <t3m15n@gmail.com>
2026-07-13 22:01:01 +0900

    コードを修正

---
+++ imageMap.js
@@ -0,0 +1,14 @@
+const GITHUB_BASE =  "https://cdn.jsdelivr.net/gh/tannbono/stamp-kun-workers/images/";
+
+const imageMap = {
+	"Ç†"           : "Ç†ÅH.png",
+	"Ç¢Ç¢Ç∆Ç®Ç‡Ç§" : "ÇªÇÍÇ¢ÇØ.png",
+	"Ç®Å[Ç‹Ç¢"     : "Ç®Å[Ç‹Ç¢.png",
+	"Ç®ÇÕÇÊÇ§"     : "Ç®ÇÕÇÊÇ§.png",
+	"Ç®Ç∆Ç»Ç»ÇÃÇ…" : "Ç®Ç∆Ç»Ç»ÇÃÇ….png",
+	"Ç≠Ç≥"         : "Ç≠Ç≥.png",
+	"Ç≥Ç¢Ç†Ç≠"     : "Ç≥Ç¢Ç†Ç≠.png",
+	"Ç»Ç´Ç‹ÇµÇΩ"   : "Ç»Ç´Ç‹ÇµÇΩ.png",
+	"Ç…Ç±"         : "Ç…Ç±Ç¡.png",
+	"Ç‡ÇƒÇ»Ç¢"     : "Ç‡ÇƒÇ»Ç¢.png",
+};
--- worker.js
+++ worker.js
@@ -1,79 +1,53 @@
-import {
-  verifyKey,
-  InteractionType,
-  InteractionResponseType
-} from "discord-interactions";
+import { verifyKey, InteractionType, InteractionResponseType } from "discord-interactions";
+import { GITHUB_BASE, imageMap } from "./imageMap.js";
 
-const GITHUB_BASE =  "https://cdn.jsdelivr.net/gh/tannbono/stamp-kun-workers/images/";
-
-const imageMap = {
-	"あ"           : "あ？.png",
-	"いいとおもう" : "それいけ.png",
-	"おーまい"     : "おーまい.png",
-	"おはよう"     : "おはよう.png",
-	"おとななのに" : "おとななのに.png",
-	"くさ"         : "くさ.png",
-	"さいあく"     : "さいあく.png",
-	"なきました"   : "なきました.png",
-	"にこ"         : "にこっ.png",
-	"もてない"     : "もてない.png",
-};
 export default {
-  async fetch(request, env) {
-
-    if (request.method !== "POST") {
-      return new Response("Discord Worker");
-    }
-
-    const signature = request.headers.get("X-Signature-Ed25519");
-    const timestamp = request.headers.get("X-Signature-Timestamp");
-
-    const body = await request.text();
-
-    const isValid = await verifyKey(
-      body,
-      signature,
-      timestamp,
-      env.DISCORD_PUBLIC_KEY
-    );
-
-    if (!isValid) {
-      return new Response("Bad request signature.", {
-        status: 401
-      });
-    }
-
-    const interaction = JSON.parse(body);
-
-    // Discordの接続確認(PING)
-    if (interaction.type === InteractionType.PING) {
-      return Response.json({
-        type: InteractionResponseType.PONG
-      });
-    }
-
-    // スラッシュコマンド
-	if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-	
-		const fileName = imageMap[interaction.data.name];
-	
-		if (fileName) {
-			const imageUrl = GITHUB_BASE + encodeURIComponent(fileName);
+	async fetch(request, env) {
+		if (request.method !== "POST") {
+			return new Response("Discord Worker");
+		}
+		
+		const signature = request.headers.get("X-Signature-Ed25519");
+		const timestamp = request.headers.get("X-Signature-Timestamp");
+		const body = await request.text();
+		const isValid = await verifyKey( body, signature, timestamp, env.DISCORD_PUBLIC_KEY );
+		
+		if (!isValid) {
+			return new Response("Bad request signature.", {
+				status: 401
+			});
+		}
+		
+		const interaction = JSON.parse(body);
+		
+		// Discordの接続確認(PING)
+		if (interaction.type === InteractionType.PING) {
+		  return Response.json({
+		    type: InteractionResponseType.PONG
+		  });
+		}
+		
+		// スラッシュコマンド
+		if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+			
+			const fileName = imageMap[interaction.data.name];
+			
+			if (fileName) {
+				const imageUrl = GITHUB_BASE + encodeURIComponent(fileName);
+				return Response.json({
+					type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+					data: {
+						content: imageUrl
+					}
+				});
+			}
 			return Response.json({
 				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 				data: {
-					content: imageUrl
+					content: "未実装のコマンドです。"
 				}
 			});
 		}
-		return Response.json({
-			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-			data: {
-				content: "未実装のコマンドです。"
-			}
-		});
+		return new Response("OK");
 	}
-
-    return new Response("OK");
-  }
 };
