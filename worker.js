56f7d18fbb06e4c5d1819954a047082f8ebe8055
tannbono <t3m15n@gmail.com>
2026-07-15 11:52:41 +0900

    コードを修正

--- worker.js
+++ worker.js
@@ -6,140 +6,128 @@ import {
 
 import { GITHUB_BASE, stamps } from "./stamps.js";
 
-async function fetchImage(url) {
-	const response = await fetch(url);
-
-	if (!response.ok) {
-		throw new Error(`画像取得失敗: ${response.status}`);
-	}
-
-	return response.arrayBuffer();
-}
-
 export default {
-  async fetch(request, env) {
-
-    if (request.method !== "POST") {
-      return new Response("Discord Worker");
-    }
-
-    const signature = request.headers.get("X-Signature-Ed25519");
-    const timestamp = request.headers.get("X-Signature-Timestamp");
-
-    const body = await request.text();
-
-    const isValid = await verifyKey(
-      body,
-      signature,
-      timestamp,
-      env.DISCORD_PUBLIC_KEY
-    );
-
-    if (!isValid) {
-      return new Response("Bad request signature.", {
-        status: 401
-      });
-    }
-
-    const interaction = JSON.parse(body);
-
-	// Discordの接続確認(PING)
-	if (interaction.type === InteractionType.PING) {
-	  return Response.json({
-	    type: InteractionResponseType.PONG
-	  });
-	}
-	//ランダム処理
-	if (interaction.data.name === "らんだむ") {
-		const stamp = stamps[Math.floor(Math.random() * stamps.length)];
-		const imageUrl = GITHUB_BASE + encodeURIComponent(stamp.file);
-		
-		return Response.json({
-			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-			data: {
-				content: imageUrl
-			}
-		});
-	}
-	if (interaction.data.name === "検索") {
-		
-		
-	return Response.json({
-		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-		data: {
-			content: "検索コマンドはここまで来ています"
+	async fetch(request, env) {
+		if (request.method !== "POST") {
+			return new Response("Discord Worker");
 		}
-	});
-		/*
-		const method = interaction.data.options.find(
-			option => option.name === "方法"
-		).value;
+		
+		const signature = request.headers.get("X-Signature-Ed25519");
+		const timestamp = request.headers.get("X-Signature-Timestamp");
+		const body = await request.text();
+		const isValid = await verifyKey(
+			body,
+			signature,
+			timestamp,
+			env.DISCORD_PUBLIC_KEY
+		);
+		
+		if (!isValid) {
+			return new Response("Bad request signature.", { status: 401 });
+		}
 
-		const value = interaction.data.options.find(
-			option => option.name === "内容"
-		).value.toLowerCase();
+		const interaction = JSON.parse(body);
 
-		let result = [];
-
-		if (method === "word") {
-
-			result = stamps.filter(stamp => {
-
-				if (stamp.name.toLowerCase().includes(value)) {
-					return true;
-				}
-
-				if (stamp.aliases?.some(alias =>
-					alias.toLowerCase().includes(value)
-				)) {
-					return true;
-				}
-
-				return false;
+		// Discordの接続確認(PING)
+		if (interaction.type === InteractionType.PING) {
+			return Response.json({
+				type: InteractionResponseType.PONG
 			});
 		}
-		else if (method === "tag") {
-
-			result = stamps.filter(stamp =>
-				stamp.tags?.some(tag =>
-					tag.toLowerCase().includes(value)
-				)
-			);
-		}
 		
-		if (result.length === 0) {
+		if (interaction.type !== InteractionType.APPLICATION_COMMAND) { return new Response("OK");}
+		const commandName = interaction.data.name;
+		
+		//ランダム処理
+		if (commandName === "らんだむ") {
+			const stamp = stamps[Math.floor(Math.random() * stamps.length)];
+			const imageUrl = GITHUB_BASE + encodeURIComponent(stamp.file);
+			
 			return Response.json({
 				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 				data: {
-					content: "該当するスタンプは見つかりませんでした。"
+					content: imageUrl
 				}
 			});
 		}
-
+		//検索
+		if (commandName === "検索"){
+			
 		return Response.json({
 			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
 			data: {
-				content:
-					`検索結果（${result.length}件）\n\n` +
-					result.map((stamp, index) =>
-						`${index + 1}. ${stamp.name}`
-					).join("\n"),
-
-				components: [{
-					type: 1,
-					components: result.slice(0, 5).map(stamp => ({
-						type: 2,
-						style: 2,
-						label: stamp.name,
-						custom_id: `stamp:${stamp.name}`
-					}))
-				}]
+				content: "検索コマンドはここまで来ています"
 			}
-		});*/
-	}
-	// スタンプ送付処理
-	if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-		const stamp = stamps.find(s => s.name === interaction.data.name);
+		});
+			/*
+			const method = interaction.data.options.find(
+				option => option.name === "方法"
+			).value;
+
+			const value = interaction.data.options.find(
+				option => option.name === "内容"
+			).value.toLowerCase();
+
+			let result = [];
+
+			if (method === "word") {
+
+				result = stamps.filter(stamp => {
+
+					if (stamp.name.toLowerCase().includes(value)) {
+						return true;
+					}
+
+					if (stamp.aliases?.some(alias =>
+						alias.toLowerCase().includes(value)
+					)) {
+						return true;
+					}
+
+					return false;
+				});
+			}
+			else if (method === "tag") {
+
+				result = stamps.filter(stamp =>
+					stamp.tags?.some(tag =>
+						tag.toLowerCase().includes(value)
+					)
+				);
+			}
+			
+			if (result.length === 0) {
+				return Response.json({
+					type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+					data: {
+						content: "該当するスタンプは見つかりませんでした。"
+					}
+				});
+			}
+
+			return Response.json({
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					content:
+						`検索結果（${result.length}件）\n\n` +
+						result.map((stamp, index) =>
+							`${index + 1}. ${stamp.name}`
+						).join("\n"),
+
+					components: [{
+						type: 1,
+						components: result.slice(0, 5).map(stamp => ({
+							type: 2,
+							style: 2,
+							label: stamp.name,
+							custom_id: `stamp:${stamp.name}`
+						}))
+					}]
+				}
+			});*/
+		}
+		// スタンプ送付処理
+		const stamp = stamps.find( s => s.name === commandName );
 		
 		if (stamp) {
 			const imageUrl = GITHUB_BASE + encodeURIComponent(stamp.file);
@@ -156,8 +144,7 @@ export default {
 				content: "未実装のコマンドです。"
 			}
 		});
+		
+	return new Response("OK");
 	}
-
-    return new Response("OK");
-  }
 };
