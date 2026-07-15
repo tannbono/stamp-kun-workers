0911a2d65142d7423a27dfc72b6daeab74e590d7
tannbono <t3m15n@gmail.com>
2026-07-15 09:29:57 +0900

    コードを修正

---
+++ register.js
@@ -0,0 +1,28 @@
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
+import { stamps } from "./imageMap.js";
+import "dotenv/config";
+
+const commands = stamps.map(stamp =>
+	new SlashCommandBuilder()
+		.setName(stamp.name)
+		.setDescription(stamp.description)
+		.toJSON()
+);
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+
+try {
+	console.log("コマンドを登録しています...");
+
+	await rest.put(
+		Routes.applicationCommands(process.env.APPLICATION_ID),
+		{
+			body: commands
+		}
+	);
+
+	console.log("コマンド登録完了！");
+}
+catch (error) {
+	console.error(error);
+}
